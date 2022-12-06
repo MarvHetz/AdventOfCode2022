061565634f8130e2468c7d3d9ec20ae30cb7ab17
Chi3ffan <116543408+MarvHetz@users.noreply.github.com>
2022-12-06 20:16:50 +0100

    Day 2 added
    Day 2 added but not finished

---
+++ Day2/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class Main
+{
+	private static ArrayList<Shape> shapesElf;
+	private static ArrayList<Shape> shapesMe;
+
+	public static void main(String[] args)
+	{
+		shapesElf = new ArrayList<>();
+		shapesMe = new ArrayList<>();
+		generateShapes();
+	}
+
+	private static void generateShapes()
+	{
+
+	}
+}
