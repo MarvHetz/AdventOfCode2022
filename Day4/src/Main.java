d7f061bca3e59283bc7d280f86ddda075a943ece
Chi3ffan <116543408+MarvHetz@users.noreply.github.com>
2022-12-08 20:31:21 +0100

    Day 4 finished

--- Day4/src/Main.java
+++ Day4/src/Main.java
@@ -5,69 +5,87 @@ import java.io.IOException;
 
 public class Main
 {
-    private static BufferedReader br;
-    private static int sumRedundantAssignments;
+	private static BufferedReader br;
+	private static int sumRedundantAssignments;
+	private static int sumOverlapingAssignments;
 
 
-    public static void main(String[] args)
-    {
-        sumRedundantAssignments = 0;
-        readFile();
+	public static void main(String[] args)
+	{
+		sumRedundantAssignments = 0;
+		sumOverlapingAssignments = 0;
+		readFile();
 
-        System.out.println("Part 1 solution: " + sumRedundantAssignments);
-    }
+		System.out.println("Part 1 solution: " + sumRedundantAssignments);
+		System.out.println("Part 2 solution: " + sumOverlapingAssignments);
+	}
 
-    private static void readFile()
-    {
-        try
-        {
-            br = new BufferedReader(new FileReader("Day 4.txt"));
-        }
-        catch (FileNotFoundException e)
-        {
-            System.out.println(e.getMessage());
-        }
+	private static void readFile()
+	{
+		try
+		{
+			br = new BufferedReader(new FileReader("Day 4.txt"));
+		}
+		catch (FileNotFoundException e)
+		{
+			System.out.println(e.getMessage());
+		}
 
-        try
-        {
-            String line;
-            String[] sections;
-            String[][] assignments = new String[2][];
-            while((line = br.readLine()) != null)
-            {
-                sections = line.split(",");
-                assignments[0] = sections[0].split("-");
-                assignments[1] = sections[1].split("-");
+		try
+		{
+			String line;
+			String[] sections;
+			String[][] assignments = new String[2][];
+			while ((line = br.readLine()) != null)
+			{
+				sections = line.split(",");
+				assignments[0] = sections[0].split("-");
+				assignments[1] = sections[1].split("-");
 
-                checkRedundant(assignments);
-            }
-        }
-        catch (IOException e)
-        {
-            System.out.println(e.getMessage());
-        }
-    }
+				checkRedundant(assignments);
+			}
+		}
+		catch (IOException e)
+		{
+			System.out.println(e.getMessage());
+		}
+	}
 
-    private static void checkRedundant(String assignments[][])
-    {
-        int assignmentsInt[][] = new int[2][2];
-        for (int i = 0; i < 2; i++)
-        {
-            for (int j = 0; j < 2; j++)
-            {
-                assignmentsInt[i][j] = Integer.valueOf(assignments[i][j]);
-                System.out.println(assignments[i][j]);
-            }
-        }
-        System.out.println();
+	private static void checkRedundant(String[][] assignments)
+	{
+		int[][] assignmentsInt = new int[2][2];
+		for (int i = 0; i < 2; i++)
+		{
+			for (int j = 0; j < 2; j++)
+			{
+				assignmentsInt[i][j] = Integer.valueOf(assignments[i][j]);
+			}
+		}
 
-        if (assignmentsInt[0][0] >= assignmentsInt[1][0] && assignmentsInt[0][1] <= assignmentsInt[1][1])
-        {
-           sumRedundantAssignments++;
-        }
-        else if (assignmentsInt[0][0] <= assignmentsInt[1][0] && assignmentsInt[0][1] >= assignmentsInt[1][1])
-        {
-            sumRedundantAssignments++;
-        }
-    }
+		if (assignmentsInt[0][0] >= assignmentsInt[1][0] && assignmentsInt[0][1] <= assignmentsInt[1][1])
+		{
+			sumRedundantAssignments++;
+		}
+		else if (assignmentsInt[0][0] <= assignmentsInt[1][0] && assignmentsInt[0][1] >= assignmentsInt[1][1])
+		{
+			sumRedundantAssignments++;
+		}
+
+		if (assignmentsInt[0][0] >= assignmentsInt[1][0] && assignmentsInt[0][0] <= assignmentsInt[1][1])
+		{
+			sumOverlapingAssignments++;
+		}
+		else if (assignmentsInt[0][1] >= assignmentsInt[1][0] && assignmentsInt[0][1] <= assignmentsInt[1][1])
+		{
+			sumOverlapingAssignments++;
+		}
+		else if (assignmentsInt[1][0] >= assignmentsInt[0][0] && assignmentsInt[1][0] <= assignmentsInt[0][1])
+		{
+			sumOverlapingAssignments++;
+		}
+		else if (assignmentsInt[1][1] >= assignmentsInt[0][0] && assignmentsInt[1][1] <= assignmentsInt[0][1])
+		{
+			sumOverlapingAssignments++;
+		}
+	}
 }
