f41fe964980f094638342836b7ee84045186d35d
MarvHetz <hetzermarvin@dv-schulen.de>
2022-12-09 11:14:31 +0100

    Day 5 some stacks added to the arraylist

--- Day 5/src/Main.java
+++ Day 5/src/Main.java
@@ -1,2 +1,21 @@
-public class Main {
+import java.io.BufferedReader;
+import java.util.ArrayList;
+
+public class Main
+{
+    private static BufferedReader br;
+    private static ArrayList<String[]> ship;
+    private static String[] stack;
+    public static void main(String[] args)
+    {
+        createStacks();
+    }
+
+    private static void createStacks()
+    {
+        stack = new String[]{"[F]","[T]","[C]","[L]","[R]","[P]","[G]","[Q]"};
+        ship.add(stack);
+        stack = new String[]{"[N]","[Q]","[H]","[W]","[R]","[F]","[S]","[J]"};
+        ship.add(stack);
+    }
 }
