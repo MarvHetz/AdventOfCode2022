4366176d5a0e9383aa604b5e180fd51b3dfbc461
Chi3ffan <116543408+MarvHetz@users.noreply.github.com>
2022-12-10 16:12:34 +0100

    Day 5 stacks added
    I created a function to add all stacks to an array of Arraylists of strings

--- Day 5/src/Main.java
+++ Day 5/src/Main.java
@@ -1,21 +1,47 @@
 import java.io.BufferedReader;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Main
 {
-    private static BufferedReader br;
-    private static ArrayList<String[]> ship;
-    private static String[] stack;
-    public static void main(String[] args)
-    {
-        createStacks();
-    }
+	private static BufferedReader br;
 
-    private static void createStacks()
-    {
-        stack = new String[]{"[F]","[T]","[C]","[L]","[R]","[P]","[G]","[Q]"};
-        ship.add(stack);
-        stack = new String[]{"[N]","[Q]","[H]","[W]","[R]","[F]","[S]","[J]"};
-        ship.add(stack);
-    }
+	private static ArrayList<String>[] ship;
+
+	public static void main(String[] args)
+	{
+		ship = new ArrayList[9];
+		createStacks();
+	}
+
+	private static void createStacks()
+	{
+		List<String> list = Arrays.asList("[F]", "[T]", "[C]", "[L]", "[R]", "[P]", "[G]", "[Q]");
+		ship[0].addAll(list);
+
+		list = Arrays.asList("[N]", "[Q]", "[H]", "[W]", "[R]", "[F]", "[S]", "[J]");
+		ship[1].addAll(list);
+
+		list = Arrays.asList("[F]", "[B]", "[H]", "[E]", "[P]", "[M]", "[Q]");
+		ship[2].addAll(list);
+
+		list = Arrays.asList("[V]", "[S]", "[T]", "[D]", "[F]");
+		ship[3].addAll(list);
+
+		list = Arrays.asList("[Q]", "[L]", "[D]", "[W]", "[V]", "[F]", "[Z]");
+		ship[4].addAll(list);
+
+		list = Arrays.asList("[Z]", "[C]", "[L]", "[S]");
+		ship[5].addAll(list);
+
+		list = Arrays.asList("[Z]", "[B]", "[M]", "[V]", "[D]", "[F]");
+		ship[6].addAll(list);
+
+		list = Arrays.asList("[T]", "[J]", "[B]");
+		ship[7].addAll(list);
+
+		list = Arrays.asList("[Q]", "[N]", "[B]", "[G]", "[L]", "[S]", "[P]", "[H]");
+		ship[8].addAll(list);
+	}
 }
